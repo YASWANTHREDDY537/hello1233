7a46d20124b6024a7a1740814a22a9ed4d5b6c51
pc495 <pc495@Devil>
2024-02-09 19:00:14 +0530

    hello

---
+++ Programind/src/proga/Fot.java
@@ -0,0 +1,19 @@
+package proga;
+
+
+public class Fot {
+	public static void main(String[] args) {
+		
+	int i =0;
+
+	for( i=0;i<=2;i++)
+	{
+		System.out.println("helo");
+		
+		for(int i1=0;i1<=2;i1++) {
+			
+			System.out.println("hi");
+		}
+	}
+	}
+}
---
+++ Programind/src/proga/Trac.java
@@ -0,0 +1,14 @@
+package proga;
+
+public class Trac {
+
+	public static void main(String[] args) {
+		int a=10;
+		for(int i=1;i<=2;i++) {
+		
+			for(int j=1;j<=2;j++) {
+				System.out.println(a++);
+			}
+		}
+	}
+}
